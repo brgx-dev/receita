ef2119ec39fa5d46f0b1398f7805b6070a76f0d1
Cleiton Borges <cleiton@digitalbgs.com>
2025-02-25 20:36:35 -0300

    created option 3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 from scripts.data_handler import handle_data_menu
 from scripts.data_unzipper import unzip_data
+from scripts.db_connection import handle_db_menu  # Importing the new function
 
 def main():
     try:
@@ -8,11 +9,14 @@ def main():
         while True: 
             print("1 - Download Dados da Receita")
             print("2 - Extrair Dados da Receita") 
+            print("3 - Connectar ao Banco")  # New option
             choice = input("Escolha uma opção: ")
             if choice == '1':
                 handle_data_menu()
             elif choice == '2':
                 unzip_data()  # Call to unzip_data without parameters
+            elif choice == '3':
+                handle_db_menu()  # Call to handle_db_menu
     except Exception as e:
         print(f"An error occurred: {e}")
 
