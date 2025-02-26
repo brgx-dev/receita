08ead464a4b487205bb0c858502ad2fea947bf30
Cleiton Borges <cleiton@digitalbgs.com>
2025-02-26 16:47:14 -0300

    v0.3.0 SQL da tabela (V1)

--- sql/data.sql
+++ sql/data.sql
@@ -1,71 +1,19 @@
-CREATE TABLE CNAE (
-    code VARCHAR(10),        -- Code for the category
-    description VARCHAR(255) -- Description of the category
+-- Tabela de logs de importação
+CREATE TABLE IF NOT EXISTS import_log (
+    id SERIAL PRIMARY KEY,
+    chunk_id INT NOT NULL,
+    table_name VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE QUALS (
-    code VARCHAR(2) PRIMARY KEY,
-    description VARCHAR(255)
-);
-
-CREATE TABLE PAIS (
-    code VARCHAR(3) PRIMARY KEY,
-    description VARCHAR(255)
-);
-
-CREATE TABLE NATJU (
-    code VARCHAR(4) PRIMARY KEY,
-    description VARCHAR(255)
-);
-
-CREATE TABLE MUNIC (
-    code VARCHAR(4) PRIMARY KEY,
-    description VARCHAR(255)
-);
-
-CREATE TABLE MOTI (
-    code VARCHAR(2),         -- Code for the reason
-    description VARCHAR(255) -- Description of the reason
-);
-
-CREATE TABLE empresas (
-    id INT PRIMARY KEY,
-    nome VARCHAR(255),
-    code1 INT,
-    code2 INT,
-    valor NUMERIC(15,2),
-    code3 INT,
-    empty VARCHAR(255) NULL
-);
-CREATE TABLE simples (
-    cnpj_basico VARCHAR(8),
-    opcao_pelo_simples VARCHAR(1),
-    data_opcao_simples DATE,
-    data_exclusao_simples DATE,
-    opcao_mei VARCHAR(1),
-    data_opcao_mei DATE,
-    data_exclusao_mei DATE
-);
-CREATE TABLE socios (
-    CNPJ TEXT,
-    Tipo_Socio INTEGER,
-    Nome_Socio TEXT,
-    Identificacao_Socio TEXT,
-    Qualificacao_Socio INTEGER,
-    Data_Entrada_Sociedade INTEGER,
-    Pais TEXT,
-    Representante_Legal TEXT,
-    Nome_Representante TEXT,
-    Faixa_Etaria TEXT,
-    Cpf_Representante TEXT
-);
-CREATE TABLE estabele (
+-- Tabela para os arquivos .ESTABELE
+CREATE TABLE IF NOT EXISTS estabelecimentos (
     cnpj_basico VARCHAR(8),
     cnpj_ordem VARCHAR(4),
     cnpj_dv VARCHAR(2),
-    identificador_matriz_filial INT,
+    matriz_filial INT,
     nome_fantasia VARCHAR(255),
-    codigo_situacao_cadastral INT,
+    situacao_cadastral INT,
     data_inicio_atividade DATE,
     cnae_fiscal_principal VARCHAR(2),
     cnae_fiscal_secundaria TEXT,
@@ -85,4 +33,77 @@ CREATE TABLE estabele (
     fax VARCHAR(255),
     email VARCHAR(255),
     situacao_especial VARCHAR(255)
+);
+
+-- Tabela para os arquivos .EMPRECSV
+CREATE TABLE IF NOT EXISTS empresas (
+    cnpj_basico INT PRIMARY KEY,
+    nome_emprsarial VARCHAR(255),
+    natureza_juridica INT,
+    qual_responsavel INT,
+    capital_social NUMERIC(15,2),
+    porte INT,
+    ente_federativo VARCHAR(255)
+);
+
+-- Tabela para os arquivos .SOCIOCSV
+CREATE TABLE IF NOT EXISTS socios (
+    cnpj_basico TEXT,
+    tipo_socio INTEGER,
+    nome_socio TEXT,
+    cpf_socio TEXT,
+    qualificacao_socio INTEGER,
+    data_entrada_sociedade INTEGER,
+    pais TEXT,
+    representante_legal TEXT,
+    nome_representante TEXT,
+    faixa_etaria TEXT
+);
+
+-- Tabela para os arquivos .SIMPLES.CSV.D50208
+CREATE TABLE IF NOT EXISTS simples (
+    cnpj_basico VARCHAR(8),
+    opcao_pelo_simples VARCHAR(1),
+    data_opcao_simples DATE,
+    data_exclusao_simples DATE,
+    opcao_mei VARCHAR(1),
+    data_opcao_mei DATE,
+    data_exclusao_mei DATE
+);
+
+
+-- Tabela para os arquivos .CNAECSV
+CREATE TABLE IF NOT EXISTS cnae (
+    codigo VARCHAR(10),
+    descricao VARCHAR(255)
+);
+
+-- Tabela para os arquivos .QUALSCSV
+CREATE TABLE IF NOT EXISTS qualificacao (
+    codigo VARCHAR(2) PRIMARY KEY,
+    descricao VARCHAR(255)
+);
+
+-- Tabela para os arquivos .PAISCSV
+CREATE TABLE IF NOT EXISTS pais (
+    codigo VARCHAR(3) PRIMARY KEY,
+    descricao VARCHAR(255)
+);
+
+-- Tabela para os arquivos .NATJUCSV
+CREATE TABLE IF NOT EXISTS natureza (
+    codigo VARCHAR(4) PRIMARY KEY,
+    descricao VARCHAR(255)
+);
+
+-- Tabela para os arquivos .MUNICCSV
+CREATE TABLE IF NOT EXISTS municipio (
+    codigo VARCHAR(4) PRIMARY KEY,
+    descricao VARCHAR(255)
+);
+
+-- Tabela para os arquivos .MOTICSV
+CREATE TABLE IF NOT EXISTS motivo (
+    codigo VARCHAR(2),
+    descricao VARCHAR(255)
 );
