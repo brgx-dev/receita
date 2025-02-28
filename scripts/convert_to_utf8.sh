9304e54a5c3fc61a8eb8715804572604867c6f83
Cleiton Borges <cleiton@digitalbgs.com>
2025-02-28 19:36:17 -0300

    v5.4 - convertion fixed

--- scripts/convert_to_utf8.sh
+++ scripts/convert_to_utf8.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-
-# Create the upload_files directory in the root of the project if it doesn't exist
-upload_dir='upload_files'
-if [ ! -d "$upload_dir" ]; then
-    mkdir "$upload_dir"
-fi
-
-for file in unzipped_csv_files/*; do
+upload_dir="upload_files"  # Define the upload directory
+cd unzipped_csv_files/
+for file in *; do
     charset=$(file -I "$file" | grep -o 'charset=[^;]*' | cut -d= -f2)
     if [[ "$charset" == "iso-8859-1" ]]; then
-        converted_file="${file%.csv}_converted.csv"
-        iconv -f ISO-8859-1 -t UTF-8 "$file" -o "$converted_file" > /dev/null
-        mv "$converted_file" "$upload_dir/"
+        converted_file="${file}_converted.csv"
+        iconv -f ISO-8859-1 -t UTF-8 -c "$file" > "$converted_file" #> /dev/null 2>&1
+        mv "$converted_file" "../$upload_dir/$file"
+        rm -f "$file"
     elif [[ "$charset" == "us-ascii" ]]; then
-        mv "$file" "$upload_dir/"  # ASCII is compatible with UTF-8
+        mv "$file" "../$upload_dir/"  # ASCII is compatible with UTF-8
     fi
 done
+# iconv -f LATIN1 -t utf-8 -c teste.csv -o teste_ok.csv > /dev/null 2>&1
--- scripts/data_unzipper.py
+++ scripts/data_unzipper.py
@@ -5,8 +5,8 @@ import os
 load_dotenv()  # Load environment variables from .env file
 
 def unzip_data():
-    download_directory = os.path.join(os.path.dirname(__file__), os.getenv("DOWNLOAD_DIRECTORY"))  # Read from .env
-    EXTRACTION_DIRECTORY = os.path.join(os.path.dirname(__file__), os.getenv("EXTRACTION_DIRECTORY"))  # Read from .env
+    download_directory = os.path.join(os.path.dirname(__file__), '../' + os.getenv("DOWNLOAD_DIRECTORY"))  # Read from .env
+    EXTRACTION_DIRECTORY = os.path.join(os.path.dirname(__file__), '../' + os.getenv("EXTRACTION_DIRECTORY"))  # Read from .env
     for zip_file in os.listdir(download_directory):
         if zip_file.endswith('.zip'):
             zip_file_path = os.path.join(download_directory, zip_file)
